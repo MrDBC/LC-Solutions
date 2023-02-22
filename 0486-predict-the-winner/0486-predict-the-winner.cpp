e091b3b1498d2e5920544ce285fa788f3ceaa4b0
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-02-23 04:04:47 +0530

    Time: 3 ms (72.17%), Space: 7.8 MB (10.72%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -6,16 +6,19 @@ public:
         for(auto e: nums)
             total_score+= e;
         
-        return dfs(nums,0,n-1)>= (total_score+1)/2;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return dfs(nums,0,n-1, dp)>= (total_score+1)/2;
     }
     
-    int dfs(vector<int>& nums, int l, int r){
+    int dfs(vector<int>& nums, int l, int r,vector<vector<int>>& dp ){
         if( l>r)
             return 0;
+        if( dp[l][r] != -1)
+            return dp[l][r];
         
-        int take_left = nums[l] + min(dfs(nums,l+2,r), dfs(nums, l+1, r-1));
-        int take_right = nums[r] + min(dfs(nums,l+1,r-1), dfs(nums,l, r-2));
+        int take_left = nums[l] + min(dfs(nums,l+2,r, dp), dfs(nums, l+1, r-1, dp));
+        int take_right = nums[r] + min(dfs(nums,l+1,r-1, dp), dfs(nums,l, r-2, dp));
         
-        return max(take_left, take_right);
+        return dp[l][r]= max(take_left, take_right);
     }
 };
