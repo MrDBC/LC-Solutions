1894f67877c8b68be4fdde9c7f95157788c905c3
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-02-23 16:20:15 +0530

    Time: 19 ms (11.55%), Space: 15.2 MB (21.71%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        vector<vector<int>> dp(prices.size(), vector<int>(2, 0));
+        vector<vector<int>> dp(prices.size()+1, vector<int>(2, 0));
         int n = prices.size();
         
         // selling on last day would give returns equal to the price of the stock on that day
-        dp[n-1][0]= prices[n-1];
+        // dp[n-1][0]= prices[n-1];
         
-        for(int idx=n-2; idx>=0 ; idx--){
+        for(int idx=n-1; idx>=0 ; idx--){
             
             // canbuy=1, means we can buy on that date
             // canbuy =0 means we can sell on that date
