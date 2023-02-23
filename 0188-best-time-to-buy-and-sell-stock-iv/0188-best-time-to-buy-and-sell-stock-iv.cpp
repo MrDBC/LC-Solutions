2380a1a83fdab2a1630b78b1845e5237e7233a30
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-02-23 17:00:22 +0530

    Time: 36 ms (24.30%), Space: 12.8 MB (60.06%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    int dp[1000][2][(100*2)+1];
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        
+        // vector<vector<vector<int>>> dp(prices.size(), vector<vector<int>>(2, vector<int>(4+1, -1)));
+        memset(dp, -1, sizeof(dp));
+        int n = prices.size();
+        
+        // my definition of transaction = either buy or sell
+        // since its given we can do at most 2 transactions: it means we can buy twice  & sell twice 
+        // so total 4 transactions
+
+        //        (prices, index, we have to buy ist, so canbuy=1 ,no of transactions we can do)
+        return dfs(prices, 0, 1, 2*k);
+    }
+        
+    int dfs(vector<int>& prices, int idx, int canbuy, int transactionsLeft ){
+       if( prices.size() == idx)
+            return 0;
+        if( transactionsLeft==0 )
+            return 0;
+        if( dp[idx][canbuy][transactionsLeft] != -1)
+            return dp[idx][canbuy][transactionsLeft];
+        
+        
+        if(canbuy )/// ie. we have option of buying, not selling
+            // 2 choices: buy current day stock or dont
+            return dp[idx][canbuy][transactionsLeft]= max( -prices[idx]+ dfs(prices, idx+1, !canbuy, transactionsLeft-1 ), dfs(prices, idx+1, canbuy, transactionsLeft));
+
+        else // we can sell and not buy, becoz we already have a stock with us
+            return  dp[idx][canbuy][transactionsLeft]= max(prices[idx]+ dfs(prices, idx+1, !canbuy, transactionsLeft-1 ), dfs(prices, idx+1, canbuy, transactionsLeft));
+        
+       
+       
+    }
+};
