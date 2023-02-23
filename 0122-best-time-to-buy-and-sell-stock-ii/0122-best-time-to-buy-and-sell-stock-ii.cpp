b4b6c3b0b0119d92b9a24d1d97543f9d6e721107
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-02-23 16:17:36 +0530

    Time: 17 ms (15.80%), Space: 15.3 MB (21.71%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        vector<vector<int>> dp(prices.size(), vector<int>(2, 0));
+        int n = prices.size();
         
-        //        ( prices, index, canbuy, dp)
-        return dfs( prices, 0, true, dp);
-    }
-    int dfs( vector<int>& prices, int idx, bool canbuy, vector<vector<int>>& dp){
-        if( idx== prices.size())
-            return 0;
-        if( dp[idx][canbuy] != -1)
-            return dp[idx][canbuy];
+        dp[n-1][0]= prices[n-1];
+        for(int idx=n-2; idx>=0 ; idx--){
+            for(int canbuy=1; canbuy>=0; canbuy--){
+                if(canbuy)/// ie. we have option of buying, not selling
+                    // 2 choices: buy current day stock or dont
+                    dp[idx][canbuy]= max( -prices[idx]+ dp[idx+1][ !canbuy] , dp[idx+1][canbuy]);
+
+                else // we can sell and not buy, becoz we already have a stock with us
+                    dp[idx][canbuy]= max(prices[idx]+  dp[idx+1][ !canbuy] , dp[idx+1][canbuy]);
+            }
+        }
         
-        if(canbuy){ /// ie. we have option of buying, not selling
-            // 2 choices: buy current day stock or dont
-            return dp[idx][canbuy]= max( -prices[idx]+ dfs(prices,idx+1, !canbuy, dp), dfs(prices, idx+1, canbuy, dp));
-        }
-        else{ // we can sell and not buy, becoz we already have a stock with us
-            return dp[idx][canbuy]= max(prices[idx]+ dfs(prices, idx+1, !canbuy, dp), dfs(prices, idx+1, canbuy,dp));
-        }
+        return dp[0][1];
+        
+       
     }
 };
