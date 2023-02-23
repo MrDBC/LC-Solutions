e7507bcc83cdf5b628f3c95cb26f9de7da51aa10
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-02-23 16:29:56 +0530

    Time: 18 ms (13.54%), Space: 17.7 MB (6.43%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -2,29 +2,27 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
-        vector<vector<int>> dp(prices.size()+1, vector<int>(2, 0));
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
         int n = prices.size();
         
-        // selling on last day would give returns equal to the price of the stock on that day
-        // dp[n-1][0]= prices[n-1];
+        return dfs(prices, 0, 1, dp);
+    }
         
-        for(int idx=n-1; idx>=0 ; idx--){
-            
-            // canbuy=1, means we can buy on that date
-            // canbuy =0 means we can sell on that date
-            for(int canbuy=1; canbuy>=0; canbuy--){
-                if(canbuy)/// ie. we have option of buying, not selling
-                    // 2 choices: buy current day stock or dont
-                    dp[idx][canbuy]= max( -prices[idx]+ dp[idx+1][ !canbuy] , dp[idx+1][canbuy]);
+    int dfs(vector<int>& prices, int idx, int canbuy, vector<vector<int>>& dp){
+       if( prices.size() == idx)
+            return 0;
+         
+        if( dp[idx][canbuy] != -1)
+            return dp[idx][canbuy];
+        
+        if(canbuy)/// ie. we have option of buying, not selling
+            // 2 choices: buy current day stock or dont
+            return dp[idx][canbuy]= max( -prices[idx]+ dfs(prices, idx+1, !canbuy , dp), dfs(prices, idx+1, canbuy, dp));
 
-                else // we can sell and not buy, becoz we already have a stock with us
-                    dp[idx][canbuy]= max(prices[idx]+  dp[idx+1][ !canbuy] , dp[idx+1][canbuy]);
-            }
-        }
+        else // we can sell and not buy, becoz we already have a stock with us
+            return dp[idx][canbuy]= max(prices[idx]+ dfs(prices, idx+1, !canbuy , dp), dfs(prices, idx+1, canbuy, dp));
         
-        // we need to buy on day 1 ( dont take it technically)
-        return dp[0][1];
-        
+       
        
     }
 };
