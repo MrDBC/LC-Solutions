b32b0b3a9fddcda5951c3b04fb987ac736058970
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-02-23 15:59:07 +0530

    Time: 22 ms (6.55%), Space: 17.2 MB (14.29%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxprofit= 0;
         
-        int prevprice = prices[0], n = prices.size();
-        for(int i=1; i<n; i++){
-            
-            if( prevprice< prices[i])
-                maxprofit+= prices[i]-prevprice;
-            
-            prevprice= prices[i];
+        vector<vector<int>> dp(prices.size(), vector<int>(2, -1));
+        
+        //        ( prices, index, canbuy, dp)
+        return dfs( prices, 0, true, dp);
+    }
+    int dfs( vector<int>& prices, int idx, bool canbuy, vector<vector<int>>& dp){
+        if( idx== prices.size())
+            return 0;
+        if( dp[idx][canbuy] != -1)
+            return dp[idx][canbuy];
+        
+        if(canbuy){ /// ie. we have option of buying, not selling
+            // 2 choices: buy current day stock or dont
+            return dp[idx][canbuy]= max( -prices[idx]+ dfs(prices,idx+1, !canbuy, dp), dfs(prices, idx+1, canbuy, dp));
         }
-        
-        return maxprofit;
+        else{ // we can sell and not buy, becoz we already have a stock with us
+            return dp[idx][canbuy]= max(prices[idx]+ dfs(prices, idx+1, !canbuy, dp), dfs(prices, idx+1, canbuy,dp));
+        }
     }
 };
