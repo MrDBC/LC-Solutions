514c7d442a528762f8560bdefcd8f1ce72496a67
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-04 15:24:08 +0530

    Time: 35 ms (18.43%), Space: 8.8 MB (50.34%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,34 +1,47 @@
 class Solution {
     vector<int> dirs{2 ,1, -2, -1, 2, -1, -2, 1, 2};
-    unordered_map<string, double> dp;
+    // double dp[25][25][101];
 public:
-    // no TLE: top-down 
+    // no TLE: bottom-down 
     // TC & SC: O( n^2 * k)
     double knightProbability(int n, int k, int row, int column) {
         
-        return dfs(n, k, row, column);
-    }
-    
-    double dfs(int n, int k, int r, int c){
-        // out of bounds check
-        if(k==0)
-            return 1;
-
-        string key = to_string(r)+" "+to_string(c)+" "+to_string(k);
-        if( dp.count(key) != 0)
-            return dp[key];
+        vector<vector<vector<double>>> dp(n, vector<vector<double>> (n, vector<double>(k+1, 0)));
+        // we are initially standing at chess[row][column] and so even if we have 0 moves left,
+        // we having probability =1, beoz we are already standing there
         
-        double prob=0;
-        for(int i=0; i<8; i++){
-            int next_row = r+ dirs[i];
-            int next_col = c+ dirs[i+1];
+        dp[row][column][0] = 1;  
+        
+        
+        for(int i=0; i<k; i++){
+            for(int r=0; r<n; r++){
+                for(int c=0; c<n; c++){
 
-            if( min(next_row, next_col)<0 or max(next_row, next_col)>=n)
-                continue;
-            prob+= ((double)1/8) * dfs(n, k-1, next_row, next_col);
+                    for(int d=0; d<8; d++){
+                        int next_row = r+ dirs[d];
+                        int next_col = c+ dirs[d+1];
+
+                        // becoz the next move is so random, and non linear,
+                        // instead of moving from chess[r][c] to chess[next_r][next_c]
+                        // we assume, we want to jump from chess[next_r][next_c] to chess[r][c]
+                        // this way we would have pre-computed our values, so it gets our job done
+                        if( min(next_row, next_col)>=0 and max(next_row, next_col)<n)
+                            dp[next_row][next_col][i+1] += ((double)1/8) *dp[r][c][i] ;
+                    }
+                }
+            }
         }
         
-        return dp[key]= prob;
+        double ans = 0;
         
+        // for given k moves, we calculate the probabilities of each cell
+        // of the chess, in our answer and return it
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                ans += dp[i][j][k];
+            }
+        }
+        return ans;
+
     }
 };
