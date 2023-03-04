0fecb1f1b6535b0e86b681a69429ff9e34aa74a3
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-04 14:23:57 +0530

    Time: 420 ms (5.22%), Space: 17.3 MB (12.38%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,13 +1,11 @@
 class Solution {
     vector<int> dirs{2 ,1, -2, -1, 2, -1, -2, 1, 2};
-    double dp[25][25][101];
+    unordered_map<string, double> dp;
 public:
     // no TLE: top-down 
     // TC & SC: O( n^2 * k)
     double knightProbability(int n, int k, int row, int column) {
-        // -2 1 2 -1 -2 -1 2 1 -2  
-        // 0 1 0 -1 0
-        memset(dp, 0, sizeof(dp));
+        
         return dfs(n, k, row, column);
     }
     
@@ -16,8 +14,9 @@ public:
         if(k==0)
             return 1;
 
-        if( dp[r][c][k] != 0)
-            return dp[r][c][k];
+        string key = to_string(r)+" "+to_string(c)+" "+to_string(k);
+        if( dp.count(key) != 0)
+            return dp[key];
         
         double prob=0;
         for(int i=0; i<8; i++){
@@ -29,7 +28,7 @@ public:
             prob+= ((double)1/8) * dfs(n, k-1, next_row, next_col);
         }
         
-        return dp[r][c][k]= prob;
+        return dp[key]= prob;
         
     }
 };
