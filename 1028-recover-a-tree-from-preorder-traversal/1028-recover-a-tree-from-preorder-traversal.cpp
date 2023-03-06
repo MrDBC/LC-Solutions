f4c6eeceb5a5886133b7a784ec6c335b866a6728
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-06 18:22:36 +0530

    Time: 27 ms (41.95%), Space: 19.5 MB (74.43%) - LeetHub

--- 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
+++ 1028-recover-a-tree-from-preorder-traversal/1028-recover-a-tree-from-preorder-traversal.cpp
@@ -19,15 +19,14 @@ public:
             
             // calculate the depth
             int depth =0;
-            while( tr[i]=='-')
-                ++depth, ++i;
+            while( tr[i]=='-' and ++i)
+                ++depth;
             
-            // calculate the no
+            // calculate the integer number
             int num =0;
             while( i<tr.size() && tr[i]!= '-')
                 num = num*10 + ( tr[i++] - '0' );
             
-            // cout<<depth<<" "<<num<<endl;
             // create a node with value = num
             TreeNode* node= new TreeNode(num);
             
@@ -38,14 +37,18 @@ public:
             while( !st.empty() and st.size() > depth)
                 st.pop();
             
+            // if parent's left child is null, assign current child "node" as left child
             if( !st.empty() and st.top()->left == NULL)
                 st.top()->left = node;
+            // if parent's left child is already assigned, it wont be null, right
+            // so u need to assign parent's right child = current "node"
             else if( !st.empty() and st.top()->left != NULL)
                 st.top()->right = node;
             
             st.push(node);
         }
         
+        // we need to return root node, so pop all other nodes first
         while( st.size()> 1)
             st.pop();
         return st.top();
