facda110f5f0a8d5363dec38e144ca14cdf56f8c
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-07 13:30:00 +0530

    Time: 83 ms (15.43%), Space: 15.4 MB (8.42%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,6 +1,11 @@
 class Solution {
+    // int dp[1001][1001];
     vector<vector<int>> dp;
 public:
+    // rephrase the PS: get the lcs for which the sum of non-lcs
+    // characters in s1 & s2 combined is minimum
+    
+    // no tle: recursion + memo = top-down dp : max(m,n)*2
     int minimumDeleteSum(string s1, string s2) {
         int m= s1.size(), n= s2.size();
         
@@ -10,22 +15,22 @@ public:
     }
     
     int deleteNonLcs(string& s1, string& s2, int m, int n){
-        if (dp[m][n] != -1) {
+        if( dp[m][n] != -1)
             return dp[m][n];
-        }
+        
         if( !m and !n )
             return 0;
         if(!m)
             return accumulate(s2.begin(), s2.begin()+n, 0);
         if( !n)
             return accumulate(s1.begin(), s1.begin()+m, 0);
+       
         
-        int ans = 0;
         if( s1[m-1] == s2[n-1])
-            ans = 0 + deleteNonLcs(s1,s2,m-1,n-1);
+            return dp[m][n] = 0+ deleteNonLcs(s1,s2,m-1,n-1);
         else
-            ans = min( int(s1[m-1]) + deleteNonLcs(s1,s2,m-1,n) , int(s2[n-1]) + deleteNonLcs(s1,s2,m,n-1));
+            return dp[m][n] = min( int(s1[m-1]) + deleteNonLcs(s1,s2,m-1,n) , int(s2[n-1]) + deleteNonLcs(s1,s2,m,n-1));
         
-        return dp[m][n] = ans;
+     
     }
 };
