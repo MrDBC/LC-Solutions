7150159260fb793a9a43fcfdfa417ae5d192c24f
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-07 12:06:33 +0530

    Time: 432 ms (47.09%), Space: 94.6 MB (33.40%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
-    long   find( vector<int>& time, long mid){
-        long takes =0;
-        for( int i=0; i< time.size(); i++){
-            takes += mid/ time[i];
-        }
-        return takes;
+    long long findTotalTripsCovered(vector<int>& time, long long int timeEachBusGets){
+        
+        long long totalTripsCovered =0;
+        for(auto t: time)
+            totalTripsCovered+= (long long)timeEachBusGets / t;
+            
+        return totalTripsCovered;
     }
+    
     long long minimumTime(vector<int>& time, int totalTrips) {
         int n= time.size();
         
-        sort(time.begin(), time.end());
-        
-        long high = (long long)(*time.begin()) * totalTrips;
-        
-        long low =1, mid;
-        
-        while( low < high){
-             mid =low+ (high - low)/2;
+        long long time_start = 1, time_end= 10e13;// (long long)(*min_element(begin(time), end(time))) * totalTrips; 
+        while( time_start < time_end){
+            long long mid_time = time_start + (time_end - time_start )/2;
+            
+            // cout<<time_start<<" "<<mid_time<<" "<<time_end<<endl;
+            // check the total trips all the buses can travel in mid_time units/hrs
+            long long trips_covered = findTotalTripsCovered(time, mid_time);
+            
+            if(trips_covered >= totalTrips ){
                 
-            long curr_takes= find(time, mid);
-            cout<< curr_takes<<endl;
-            if( curr_takes >= totalTrips )
-                high= mid;
-            else 
-                low= mid+1;
+                time_end = mid_time;
+            }
+            else
+                time_start= mid_time+1;
             
         }
-        return low;
+        
+        return time_end;
     }
 };
