fa18b83475b2b817a216506c32e9187c1ca91f4f
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-08 13:48:17 +0530

    Time: 51 ms (9.26%), Space: 16.9 MB (13.81%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -3,31 +3,19 @@ class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
         int n= stones.size();
-        sort(begin(stones), end(stones));
-    
-        // range of sum can be +- ( 30*100)
-        // so total types of sum= 2*( 30*100)   + 1(for safety)
-        dp.resize(n+1, vector<int>(6000+1, -1));
+       
+        unordered_set<int> us{0};
         
-        // 1 1 2 4 7 8
-        return dfs(stones, n);
-    }
-    int dfs(vector<int>& stones, int n, int sum=0 ){
-        if( n==0)
-            return (sum<0)?100: sum; // we dont want to choose negative sum 
+        for(auto stone: stones){
+            unordered_set<int> temp;
+            
+            for(auto x: us)
+                temp.insert({x-stone, x+ stone});
+            us=temp;
+        }
         
-        // our sum can be negative, but we cant have negative indices
-        // so we need a uniform formula for converting those negative
-        // sums to positive
-        // we know the minimum sum = -(30*100) or more specifically -(stones.size() * 100 )
-        
-        int mod_sum = sum+ (stones.size()* 100);
-        if( dp[n][mod_sum] != -1)
-            return dp[n][mod_sum];
-        
-        int pos= dfs(stones, n-1, sum+ stones[n-1]);
-        int neg =  dfs(stones,n-1, sum -stones[n-1]);
-        
-        return dp[n][mod_sum]=min( pos, neg);
+        return abs(*min_element(begin(us), end(us), [](int a, int b){
+            return abs(a)<abs(b);
+        }));
     }
 };
