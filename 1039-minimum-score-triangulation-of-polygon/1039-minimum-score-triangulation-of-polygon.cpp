80499f0d21b290421053cc14568286e0967f0da0
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-08 19:22:22 +0530

    Time: 12 ms (36.71%), Space: 8.5 MB (85.13%) - LeetHub

--- 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
+++ 1039-minimum-score-triangulation-of-polygon/1039-minimum-score-triangulation-of-polygon.cpp
@@ -1,33 +1,29 @@
 class Solution {
-    int dp[50][50];
+    // int dp[50][50];
 public:
     int minScoreTriangulation(vector<int>& values) {
         int n= values.size();
-        memset(dp, -1, sizeof(dp));
-        return  domcm(values, 0, n-1);
-    }
-    int domcm(vector<int>& values, int i, int j){
         
-        // atleast 3 vertices needed to make a polygon( triangle)
-        if( (j-i) < 2)
-            return 0;
-        
-        if( dp[i][j] != -1)
-            return dp[i][j];
-        
-        int minscore = INT_MAX;
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+  
         // fix vertices i & j and select a 3 vertex lying in range (i, j)  
         // [non- including i & j ofc \U0001f602]
         // form a triangle using (i , k, j) and then 
         // recurse for the sub-polygons
         
-        for(int k=i+1; k<j; k++){
-            
-            // score of forming triangle (i, k, j)
-            int tri_score = (values[i]* values[k] * values[j] ) ;
-            minscore = min( minscore, tri_score+ domcm(values, i,k) + domcm(values, k,j) );
+        for(int i=n-3; i>=0; i--){
+            for( int j= i+2; j<n; j++){
+                
+                dp[i][j] = INT_MAX;
+                for(int k=i+1; k<j; k++){
+                    
+                    int tri_score = (values[i]* values[k] * values[j] ) ;
+                    dp[i][j]= min( dp[i][j] , tri_score+ dp[i][k] + dp[k][j] );
+                }
+            }
         }
-        
-        return dp[i][j] = minscore;
+
+            
+        return dp[0][n-1];
     }
 };
