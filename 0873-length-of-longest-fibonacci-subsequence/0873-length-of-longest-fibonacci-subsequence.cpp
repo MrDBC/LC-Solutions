9913ee50d385d783029d436bf44973fb67e2f80f
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-09 03:04:51 +0530

    Time: 613 ms (54.85%), Space: 12.1 MB (74.85%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -32,38 +32,3 @@ public:
         
     }
 };
-
-// class Solution {
-// public:
-    
-//     int find(vector<int>&A, unordered_map<int,int>&m, vector<vector<int> >&dp, int cur, int prev) {
-        
-//         // if(dp[cur][prev]!=-1)
-//         //     return dp[cur][prev];
-        
-//         int ans = 2;
-//         if(m.find(A[cur]+A[prev])!=m.end()) {
-//             int new_pos = m[A[cur]+A[prev]];
-//             ans = max(ans, 1 + find(A,m,dp,new_pos,cur));
-//         }
-//         // dp[cur][prev]=ans;
-//         return ans;
-//     }
-//     int lenLongestFibSubseq(vector<int>& A) {
-//         vector<vector<int> >dp(A.size(), vector<int>(A.size(),-1));
-//         unordered_map<int,int>m;
-//         for(int i=0;i<A.size();i++) {
-//             m[A[i]]=i;
-//         }
-//         int ans = 0;
-//         for(int i=0;i<A.size();i++) {
-//             for(int j=i+1;j<A.size();j++) {
-//                 if(m.find(A[i]+A[j])!=m.end()) {
-//                     int new_pos = m[A[i]+A[j]];
-//                     ans = max(ans, 1 + find(A,m,dp,new_pos,j));
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
