c66c27730b8b7bc129a9e307da0ccea922639efe
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-09 03:11:17 +0530

    Time: 786 ms (38.79%), Space: 145.9 MB (19.70%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.cpp
@@ -1,34 +1,23 @@
 class Solution {
-    unordered_map<int, int> index;
-    int dp[1001][1001];
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
-        int n= arr.size();
+        int n = arr.size();
+        unordered_map<int, int> map;
+        vector<vector<int>> matrix (n, vector<int> (n, -1));
+        for(int i = 0; i < n; i++)
+            map[arr[i]] = i;
+        int maxS = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++)
+                maxS = max(maxS, dp(i, j,  map, matrix, arr));
+        return maxS == 0 ? 0 : 2 + maxS;
         
-        for(int i=0; i<n; i++)
-            index[arr[i]]=i;
-        
-        memset(dp, -1, sizeof(dp));
-        int maxlen = 0;
-        
-        // (a, b , c)
-        for(int a=0; a<= n-3; a++)
-            for(int b=a+1; b<=n-2; b++)
-                maxlen = max(maxlen, dfs(arr, a, b));
-       
-        return (maxlen !=0)? maxlen +2: 0 ;
     }
-    int dfs( vector<int>& arr, int a, int b){
-        if( dp[a][b] != -1)
-            return dp[a][b];
-        
-        int val_c = (arr[a] + arr[b]) ;
-        
-        if( index.find(val_c) != index.end() )
-            return  dp[a][b]= 1+ dfs(arr, b, index[val_c]);
-        else
-            return dp[a][b]= 0;
-        
-        
+    
+    int dp(int i, int j, unordered_map<int, int>& map, vector<vector<int>>& matrix, vector<int>& arr)
+    {
+        if (matrix[i][j] != -1)                         return matrix[i][j];
+        if (map.find(arr[i] + arr[j]) == map.end())     return matrix[i][j] = 0;
+        return matrix[i][j] = 1 + dp(j,  map[arr[i] + arr[j]], map, matrix, arr);
     }
 };
