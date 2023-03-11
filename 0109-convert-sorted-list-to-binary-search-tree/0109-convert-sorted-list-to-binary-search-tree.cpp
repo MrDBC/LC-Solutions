05d2cab5f5c4435669bead5f48c63475883aefdb
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-11 10:16:28 +0530

    Time: 29 ms (61.10%), Space: 28.3 MB (92.35%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -22,18 +22,26 @@
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head, ListNode* end= nullptr) {
+        // just note end is not our end within our current window
+        // it is the starting of the next window
+        // basically in our current winodw, we start at head & finish at (end-1)
+        
+        // this means we have crossed our window, so return nullptr
         if( head== end)
             return nullptr;
         
+        // just to get the middle node
         ListNode* slow = head, *fast = head;
         while( fast != end and fast->next!= end){
             slow = slow->next;
             fast=fast->next->next;
         }
         
+        // after gettting middle listnode, make it a treenode 
+        // and recursively form its left and right subtree
         TreeNode* root = new TreeNode(slow->val);
-        root->left = sortedListToBST(head, slow);
-        root->right = sortedListToBST(slow->next, end);
+        root->left = sortedListToBST(head, slow); // our window = [head.....(slow-1)]
+        root->right = sortedListToBST(slow->next, end); // our window = [slow+1.... end-1]
         
         return root;
     }
