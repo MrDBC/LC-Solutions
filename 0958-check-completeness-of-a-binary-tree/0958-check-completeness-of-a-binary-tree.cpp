ee8edb62daf95e97ee03c2559deb745f9540993a
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-15 10:50:46 +0530

    Time: 3 ms (84.99%), Space: 10.6 MB (10.56%) - LeetHub

---
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        int h=0;
+        while( !q.empty()){
+            int currLevelNodes = pow(2,h);
+            
+            int sz = q.size();
+            bool incompleteNode = false;
+            for(int i=0; i<sz; i++){
+                TreeNode* curr = q.front();
+                --currLevelNodes;
+                q.pop();
+                    
+                // at the same level, we have nodes to the left which are incomplete
+                // and this node(on the right) has children, return false
+                if( (incompleteNode and ( curr->right or curr->left)) || (!curr->left and curr->right))
+                    return false;
+                
+                // just checking whether any node to the left doesnt have 2 children
+                if( !curr->left or !curr->right)
+                    incompleteNode = true;
+                
+                if( curr->left)
+                    q.push(curr->left);
+                if (curr->right)
+                    q.push(curr->right);
+                
+            }
+            ++h;
+            if( !q.empty() and currLevelNodes!=0)
+                return false;
+        }
+        
+        return true;
+    }
+};
