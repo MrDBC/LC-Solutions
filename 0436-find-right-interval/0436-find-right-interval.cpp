95eccd14fac0d3f10e857ab9fab3198c73490e6d
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-16 12:16:25 +0530

    Time: 79 ms (75.40%), Space: 27.9 MB (55.85%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.cpp
+++ 0436-find-right-interval/0436-find-right-interval.cpp
@@ -2,19 +2,24 @@ class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
         
-        int n= intervals.size();
+        int n = intervals.size();
+        vector<pair<int,int>> map;
         
-        vector<int> ans(n);
-        map<int, int> m;
+        for(int i = 0; i < n; i++) 
+            map.push_back({intervals[i][0],i});
         
-        /// mapping starting value of an interval to its index
-        for(int i = 0; i < intervals.size(); i++)
-            m[intervals[i][0]] = i;
+        sort(map.begin(), map.end());
         
-        for(int i = 0; i < intervals.size(); i++) {
-            auto itr = m.lower_bound(intervals[i][1]);
-            ans[i]  = itr!=end(m)? itr-> second: -1;
+        vector<int> ans;
+        for(auto v : intervals) {
+        
+            // custome lower_bound, signs are opposite
+            // ig other than vector or pair
+            auto it = lower_bound(map.begin(), map.end(), v[1], [](pair<int, int>& a, int b) { return a.first < b;});
+         
+            ans.push_back(it == map.end() ? -1 : it->second);
         }
+        
         return ans;
     }
 };
