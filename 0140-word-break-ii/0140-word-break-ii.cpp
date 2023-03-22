7ab3b9655145b71b9e2a19d1a10cd5d859af702e
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-22 15:08:18 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (64.68%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,76 +1,79 @@
-// class Solution {
-//     vector<string> res;
-// public:
-//     vector<string> wordBreak(string s, vector<string>& wordDict) {
-//         unordered_set<string> us;
-//         for(auto str:wordDict)
-//             us.insert(str);
-        
-//         string word="", sentence="";
-//         dfs( s, 0, word, sentence, us);
-//         return res;
-//     }
-    
-//     void dfs( string& s, int idx, string& word, string& sentence, unordered_set<string>& us ){
-//         if( idx>s.size())
-//             return ;
-//         if( idx== s.size()){
-//             res.push_back(sentence);
-//             return;
-//         }
-        
-//         // idx =3
-//         // cat sanddog
-//         // 012 3456789
-//         if( us.find(word) != us.end()){ //cat
-//             // sentence+= word+" ";
-//             string tempword="";
-//             string tempsentence= sentence+word+" ";
-//             dfs(s, idx, tempword, tempsentence,us); // sentence ="cat and " rem="dog"
-            
-            
-//             dfs(s,idx, word, sentence, us ); 
-//         }
-//         else{
-//             word+= s[idx]; // and
-//             dfs(s, idx+1, word, sentence, us);
-//         }
-//     }
-// };
-
 class Solution {
+    vector<string> res;
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        //insert all the words in the set
-        unordered_set<string> set;
-        vector<string> res;
-        for(auto word:wordDict)
-            set.insert(word);
-        //to store the current string 
-        string curr="";
-        findHelper(0,s,curr,set,res);
+        unordered_set<string> us;
+        for(auto str:wordDict)
+            us.insert(str);
+        
+        string word="", sentence="";
+        dfs( s, 0, word, sentence,0, us);
         return res;
     }
     
-    void findHelper(int ind,string s,string curr,unordered_set<string> set,vector<string>& res)
-    {
-        if(ind==s.length())
-        {
-            //we have reached end
-            curr.pop_back(); //remove the trailing space
-            res.push_back(curr);
-        }
-        string str="";
-        for(int i=ind;i<s.length();i++)
-        {
-            //get every substring and check if it exists in set
-            str.push_back(s[i]);
-            if(set.count(str))
-            {
-                //we have got a word in dict 
-                //explore more and get other substrings
-                findHelper(i+1,s,curr+str+" ",set,res);
+    void dfs( string& s, int idx, string word, string sentence, int cnt, unordered_set<string>& us ){
+       
+        if( idx== s.size() ){
+            if(sentence.size()== s.size()+cnt){
+                if( sentence.size())
+                    sentence.pop_back();
+                res.push_back(sentence);
             }
+            return;
         }
+        
+        // idx =3
+        // cat sanddog
+        // 012 3456789
+        word+=s[idx];
+        dfs(s, idx+1, word, sentence, cnt, us);
+        if( us.find(word) != us.end()){ //cat
+            // sentence+= word+" ";
+            // string tempword="";
+            // string tempsentence= ;
+            dfs(s, idx+1, "", sentence+word+" ", cnt+1,us); // sentence ="cat and " rem="dog"
+        }
+        
+        // word+= s[idx]; // and
+        // else
+            
+        
     }
 };
+
+// class Solution {
+// public:
+//     vector<string> wordBreak(string s, vector<string>& wordDict) {
+//         //insert all the words in the set
+//         unordered_set<string> set;
+//         vector<string> res;
+//         for(auto word:wordDict)
+//             set.insert(word);
+//         //to store the current string 
+//         string curr="";
+//         findHelper(0,s,curr,set,res);
+//         return res;
+//     }
+    
+//     void findHelper(int ind,string s,string curr,unordered_set<string> set,vector<string>& res)
+//     {
+//         if(ind==s.length())
+//         {
+//             //we have reached end
+//             curr.pop_back(); //remove the trailing space
+//             res.push_back(curr);
+//         }
+//         string str="";
+//         for(int i=ind;i<s.length();i++)
+//         {
+//             //get every substring and check if it exists in set
+//             str.push_back(s[i]);
+//             if(set.count(str))
+//             {
+//                 //we have got a word in dict 
+//                 //explore more and get other substrings
+//                 findHelper(i+1,s,curr+str+" ",set,res);
+//             }
+//         }
+//     }
+// };
