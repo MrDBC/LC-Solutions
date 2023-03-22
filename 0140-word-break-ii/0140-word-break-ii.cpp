a9b3a2946a8509b8d8606f9c2aa622553adab0bd
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-22 15:21:36 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (85.10%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -6,30 +6,26 @@ public:
         for(auto str:wordDict)
             us.insert(str);
     
-        dfs( s, 0, "", "",0, us);
+        dfs( s, 0, "", us);
         return res;
     }
     
     // cnt is the count of spaces we insert in our sentence
-    void dfs( string& s, int idx, string word, string sentence, int cnt, unordered_set<string>& us ){
+    void dfs( string& s, int idx, string sentence, unordered_set<string>& us ){
        
         if( idx== s.size() ){
-            
-            // this confirms we have included every character in our original "s" into our "sentence" as well
-            if(sentence.size() == s.size()+cnt){
-                sentence.pop_back();
-                res.push_back(sentence);
-            }
+            sentence.pop_back();
+            res.push_back(sentence);
             return;
         }
         
-        word+=s[idx];
-        
-        // our dictionary contains "word", so add it to the sentence, and reinitialize word to empty string
-        if( us.find(word) != us.end())
-            dfs(s, idx+1, "", sentence+word+" ", cnt+1,us); 
-        
-        // we want to see other options
-        dfs(s, idx+1, word, sentence, cnt, us);
+        string newword="";
+        for(int i=idx; i<s.size(); i++){
+            newword+= s[i];
+            
+            if( us.count(newword)){
+                dfs(s, i+1, sentence+newword+" ", us);
+            }
+        }
     }
 };
