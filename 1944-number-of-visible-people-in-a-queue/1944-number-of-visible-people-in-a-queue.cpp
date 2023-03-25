3190cd0a1a00f2dfffe72a99527e4c3416058add
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-25 23:15:53 +0530

    Time: 196 ms (66.73%), Space: 84.2 MB (57.54%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -6,14 +6,8 @@ public:
         // the stack is a strictly monotonic increasing one
         stack<int> st;
         
-        vector<int> res(n,0);
+        vector<int> res(n);
         for(int i=n-1; i>=0 ; i--){
-    
-            // stack is already monotonic increasing one 
-            // even after insertion of currrent height
-            // so its obvious we can only see the next height only
-            // if( !st.empty() and st.top()> heights[i])
-            //     res[i]=1;
             
             // pop all the heights having heights <= current height
             // now the total heights we can see is the no. of popped 
@@ -21,11 +15,9 @@ public:
             // case 1: +1 , if the stack is non empty, after popping
             // case 2: +0 , if the stack is empty , after popping
             int howmany=0;
-            while( !st.empty() and st.top()<= heights[i]){
+            while( !st.empty() and st.top()<= heights[i] and ++howmany)
                 st.pop();
-                ++howmany;
-            }
-            // if( howmany>0)
+                
             res[i]= howmany+ (!st.empty()? 1: 0);
 
             // push current height into the stack
