4347a7114b1502db951f0747f2d8dc26aa08a650
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-03-28 11:32:47 +0530

    Time: 19 ms (5.02%), Space: 9.8 MB (23.29%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,24 +1,52 @@
 class Solution {
+    unordered_set<int> setdays;
     int dp[365+1];
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
+        for(auto d: days)
+            setdays.insert(d);
         
-        int n= days.size();
-        memset(dp, 0, sizeof(dp));
+        memset(dp, -1, sizeof(dp));
+        return dfs(1,costs );
+    }
+    int dfs(int curr_day, vector<int>& costs){
+        if( curr_day>365)
+            return 0;
         
-        // 1 2 3 4 5 6 7 8
-        unordered_set<int> travelDays(begin(days), end(days));
-        for(int day=1; day<=365; day++){
-            if( travelDays.count(day)){
-                dp[day]= min( costs[0] + dp[day-1], min(
-                                costs[1] + (day>=7? dp[day-7] : 0), 
-                                costs[2] + (day>=30? dp[day-30] : 0)));
+        if( dp[curr_day] != -1)
+            return dp[curr_day];
+        // 3 choices: 1 , 7 or 30 day pass
+        int cost1=INT_MAX, cost7= INT_MAX, cost30= INT_MAX;
+        if( setdays.find(curr_day) != setdays.end())
+            cost1= costs[0] + dfs(curr_day+1, costs);
+        else
+            cost1 = dfs(curr_day+1, costs);
+        
+        for(int i=0; i<7; i++){
+            int day= curr_day+i;
+            if( setdays.find(day) != setdays.end()){
+                cost7= costs[1] + dfs(curr_day+7, costs);
+                break;
             }
-            else
-                dp[day]= dp[day-1];
         }
+        if( cost7==INT_MAX)
+            cost7=  dfs(curr_day+7, costs);
         
-        return dp[365];
         
+        for(int i=0; i<30; i++){
+            int day= curr_day+i;
+            if( setdays.find(day) != setdays.end()){
+                cost30= costs[2] + dfs(curr_day+30, costs);
+                break;
+            }
+        }
+        if( cost30 ==INT_MAX)
+            cost30 =  dfs(curr_day+30, costs);
+        
+        return dp[curr_day]= min(cost1, min(cost7, cost30));
+        // if( mincost ==INT_MAX)
+        //     return dfs(curr_day+30, costs);
+        // return mincost;
+            
     }
 };
