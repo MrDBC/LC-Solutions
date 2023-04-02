270c640cc6ba32b69d76ab7d2367b80eade456c5
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-04-02 10:30:48 +0530

    Time: 271 ms (32.01%), Space: 97 MB (99.56%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -3,17 +3,21 @@ public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
         int n= spells.size(), m = potions.size();
         
+        // sort  the potions vector, 
         sort(begin(potions), end(potions));
         
         vector<int> ans(n);
         
         for(int i=0; i<n; i++){
+            
+            // and for every spell, find the least factor we need
+            // which when multpiled with spell ( ie., spell * factor) >= success
             int spell= spells[i];
             long long factor = (success+spell-1)/spell;
             
             // 5 8 8
             auto itr = lower_bound(begin(potions), end( potions), factor);
-            ans[i]= potions.end()- itr;
+            ans[i]= potions.end()- itr; // count of such factors
         }
         
         return ans;
