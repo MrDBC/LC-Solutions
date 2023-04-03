a31632d9f32e2d1b80f1b6d25ddae523ea6edb66
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-04-03 11:25:52 +0530

    Time: 67 ms (90.23%), Space: 24.1 MB (57.72%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -5,31 +5,17 @@ public:
         int cnt=0;
         int m= matrix.size(), n= matrix[0].size();
         
-        memset(dp, -1, sizeof( dp));
+        memset(dp, 0, sizeof( dp));
         
-        for(int i=0; i<m;i ++)
-            for(int j=0; j<n; j++)
-                cnt+= dfs(i,j,matrix);
-        
+        for(int i=1; i<=m;i ++)
+            for(int j=1; j<=n; j++){
+                if( matrix[i-1][j-1])
+                    dp[i][j]= 1+min( dp[i-1][j-1], min( dp[i][j-1], dp[i-1][j]));
+                cnt+= dp[i][j];
+            }
         return cnt;
     }
     
-    // dp[i][j] = maximum length of square with bottom-right vertex at matrix[i][j]
-    int dfs( int i, int j, vector<vector<int>>& matrix){
-        int m= matrix.size(), n= matrix[0].size();
-        
-        if( dp[i][j] != -1)
-            return dp[i][j];
-        
-        if( matrix[i][j] == 1){
-            if( i and j)
-                dp[i][j]= 1+min( dfs(i-1, j-1, matrix), min( dfs( i-1, j, matrix), dfs( i, j-1, matrix)));
-            else
-                dp[i][j]= 1;
-        }
-        else
-            dp[i][j]=0;
+    // dp[i][j] = maximum length of square with bottom-right vertex at (i,j)
     
-        return dp[i][j];
-    }
 };
