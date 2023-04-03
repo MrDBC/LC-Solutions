f4a16cae96c59555bcee430059d6860b824850fb
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-04-03 10:23:51 +0530

    Time: 79 ms (90.98%), Space: 42 MB (59.84%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -5,15 +5,9 @@ public:
         
         // 1 2 2 3
         sort(people.begin(), people.end());
-        
-        // [5,1,4,2] limit =6
 
         int i=0,j=n-1, boats=0;
         while( i<=j){
-            // if( i==j){
-            //     ++boats;
-            //     break;
-            // }
             if( people[i]+people[j] <= limit)
                 ++i;
             
