d6fc16dad87b97c95d911034e29567f97b27f01f
Dhan Bahadur Chhettri <progcomp29@gmail.com>
2023-05-12 13:10:57 +0530

    Time: 394 ms (30.56%), Space: 120.4 MB (64.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -16,10 +16,8 @@ public:
         if( dp[idx] != -1)
             return dp[idx];
         
-        long long take_it=0, dont_take_it=0;
-        
-        take_it= questions[idx][0] + dfs(questions, idx+questions[idx][1]+1, n);
-        dont_take_it = dfs(questions, idx+1, n);
+        long long take_it = questions[idx][0] + dfs(questions, idx+questions[idx][1]+1, n);
+        long long dont_take_it = dfs(questions, idx+1, n);
         
         return dp[idx]= max( take_it, dont_take_it);
     }
